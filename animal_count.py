81c91d064c428228b054686f51bf35236837876b
rais-github <100830983+rais-github@users.noreply.github.com>
2022-05-26 13:58:46 +0530

    Add files via upload

---
+++ animal_count.py
@@ -0,0 +1,5 @@
+head=int(input("Enter the number of animal in the farmhouse:"))
+leg=int(input("Enter the number of legs:"))
+chick=(leg-(2*head))//2
+print("THe number of chickens in the farmhouse are:",chick)
+print("THe number of cows in the farmhouse are:",head-chick)
