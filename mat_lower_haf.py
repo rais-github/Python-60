25d5da3588c7ca01e22498a8fdc2cc74c9cc5430
rais-github <100830983+rais-github@users.noreply.github.com>
2022-05-29 01:19:50 +0530

    Add files via upload

---
+++ mat_lower_haf.py
@@ -0,0 +1,7 @@
+C,R=[int(x) for x in input("Enter rows and column:").split(" ")]
+mat = [[int(input()) for x in range (C)] for y in range(R)]
+for i in range(R):
+    for j in range(C):
+        if(i+j>=R-1):
+            print(mat[i][j],end=" ")
+        
