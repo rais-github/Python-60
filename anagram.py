83fb62bf5d0badd7e7e69f6a56c383af4bec5f06
rais-github <100830983+rais-github@users.noreply.github.com>
2022-05-29 01:12:38 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,11 @@
+def check(s1, s2):
+
+	if(sorted(s1)== sorted(s2)):
+		print("The strings are anagrams.")
+	else:
+		print("The strings aren't anagrams.")		
+		
+# driver code
+s1 ="listen"
+s2 ="silent"
+check(s1, s2)
