5b34f937fdf3bcb0831ea737acce0730e7710719
rais-github <100830983+rais-github@users.noreply.github.com>
2022-05-29 01:18:59 +0530

    Add files via upload

---
+++ floor.py
@@ -0,0 +1,7 @@
+L=int(input("Enter the length of the floor"))
+B=int(input("Enter the breadth of the floor"))
+l=int(input("Enter the length of tiles"))
+b=int(input("Enter the breadth of tiles"))
+A=L*B
+a=l*b
+print("The number of tiles required in floor are:",A/a)
